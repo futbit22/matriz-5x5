c4cd5ad99daf5241ca5f2b877f821f27a6c8e720
William Crespo <Williamfarias2004@gmail.com>
2026-02-27 09:50:41 -0500

    Tarea matriz 5x5 completada

---
+++ S11.java
@@ -0,0 +1,26 @@
+package s11;
+
+import java.util.Scanner;
+
+public class S11 {
+
+    public static void main(String[] args) {
+        Scanner s11 = new Scanner(System.in);
+        int[][] matriz = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("ingrese un valor para [" + i + "][" + j + "]:");
+                matriz[i][j] = s11.nextInt();
+
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + "\t");
+
+            }
+            System.out.println("");
+        }
+    }
+
+}
